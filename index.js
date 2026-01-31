363e3be75bf129e633db9a357699b711a52071d8
unknown <ganagallajoshitha@gmail.com>
2026-01-31 20:39:18 +0530

    Updated all the codes of controllers, middlware and routes with index.js

--- index.js
+++ index.js
@@ -1,34 +1,18 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import {supabase} from "./config/supabase.js";
-import userRoutes from "./routes/user.routes.js";
-import vehicleRoutes from "./routes/vehicle.routes.js";
-import tripRoutes from "./routes/trip.routes.js";
-
-import logger from "./middleware/logger.js";
-import notFound from "./middleware/notFound.js";
-
-dotenv.config();
-
+const express = require('express');
+const userRoutes = require('./routes/user.routes');
+const vehicleRoutes = require('./routes/vehicle.routes');
+const tripRoutes = require('./routes/trip.routes');
+const logger = require('./middleware/logger');
+require("dotenv").config();
 const app = express();
-
-app.use(cors());
 app.use(express.json());
 app.use(logger);
+app.use('/users', userRoutes);
+app.use('/vehicles', vehicleRoutes);
+app.use('/trips', tripRoutes);
+app.use((req, res) => 
+  res.status(404).json({ message: 'This Request is not found' }));
 
-app.get("/", async(req, res)=>{
-    const {data,error} = await supabase.from("users").select("*")
-    if(error) return 
-    res.status(500).json({error:error.message})
-    res.json(data)
-})
-
-app.use("/users",userRoutes);
-app.use("/vehicles",vehicleRoutes);
-app.use("/trips",tripRoutes);
-app.use(notFound);
-
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => 
+    console.log(`Server is running on port ${PORT}`));
